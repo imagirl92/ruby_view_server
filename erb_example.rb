f5377aea66d43f467325e09fd3aade81983fb675
Admin <Admin@ip-10-0-66-6.ap-southeast-1.compute.internal>
2015-10-31 08:41:22 +0700

    homework section 2 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+rabbit = "lily"
+template1 = ERB.new "The name of my rabbit is <%= rabbit %>"
+puts template1.result(binding)
+
+my_array = [23, 54, 678, 34]
+template2 = ERB.new "My first array is <%= my_array %>"
+puts template2.result(binding)
